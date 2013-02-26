89b9c64faba61a197ead13e27a4ac580dd652797
David Santiago <david.santiago@gmail.com>
2013-02-25 20:09:39 -0800

    Change criterium version to 0.3.1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject perforate "0.2.4-SNAPSHOT"
   :description "Painless benchmarking with Leiningen."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [criterium "0.2.0"]]
+                 [criterium "0.3.1"]]
   :eval-in :leiningen
   :perforate {:environments [{:namespaces [perforate.benchmarks.core]}]})

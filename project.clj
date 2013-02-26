2f62b9fbf26a6dcebea8c50e2943912c1591a590
David Santiago <david.santiago@gmail.com>
2013-02-25 20:12:26 -0800

    Update project version to 0.2.4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject perforate "0.2.4-SNAPSHOT"
+(defproject perforate "0.2.4"
   :description "Painless benchmarking with Leiningen."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [criterium "0.3.1"]]
